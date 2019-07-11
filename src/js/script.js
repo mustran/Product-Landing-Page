23d1fecdc081f0d8578ba9278e96d1aa725617a8
mustran <ademimusli000@gmail.com>
2019-07-11 04:02:22 +0200

    products section edited

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,29 @@
-let detailsButton = document.querySelector("#searchButton");
-let prod1 = document.querySelector("#prod1");
+let detailsButton1 = document.querySelector("#searchButton1");
+let detailsButton2 = document.querySelector("#searchButton2");
+let detailsButton3 = document.querySelector("#searchButton3");
 
-detailsButton.addEventListener('click', function () {
+let prod1 = document.querySelector("#prod1");
+let prod2 = document.querySelector("#prod2");
+let prod3 = document.querySelector("#prod3");
+
+detailsButton1.addEventListener('click', function () {
     prod1.classList.toggle("showDescription");
 })
+
+detailsButton2.addEventListener('click', function () {
+    // console.log("CLICKED");
+    prod2.classList.toggle("showDescription");
+})
+
+detailsButton3.addEventListener('click', function () {
+    prod3.classList.toggle("showDescription");
+})
+
+// let showHide = document.querySelectorAll(".show");
+// console.log(showHide);
+
+// showHide.forEach(element => {
+//     element.addEventListener('click', function(){
+//         element.classList.toggle("showDescription");
+//     })
+// });
